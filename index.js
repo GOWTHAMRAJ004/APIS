7425f985529fa69d0e8568b1ab1a04d9f906f204
Gowthamraj S <rgowtham@CEIIND.COM>
2025-02-20 10:01:04 +0530

    findById function renamed to findUser

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB  = require("./config/config");
 const router = require("./routers/userRouter");
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", router); 
+
+
 
 // Default Route
 app.get("/", (req, res) => {
